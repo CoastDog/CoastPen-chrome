3020ca29d23826f006c674f7a62f64d19d729876
Lap Shun Yu <yulapshun@oursky.com>
2016-03-05 17:22:12 +0800

    Server now accept web socket connection

--- background.js
+++ background.js
@@ -1,13 +1,37 @@
+var laserExtensionId = 'oahfegphkjbgdacfibhkldkikjmfjmdm';
+
 window.onload = function() {
   var server = new http.Server();
+  var wsServer = new http.WebSocketServer(server);
   server.addEventListener('request', function(req) {
     // Serve hihi.
     req.writeHead(200, {
       'Content-Type': 'text/plain',
       'Content-Length': 5});
     req.end('hihi\n');
-    var laserExtensionId = 'llkmhohmbogloloockknondominpdlap';
     chrome.runtime.sendMessage(laserExtensionId, {getTargetData: true});
+    return true;
+  });
+  var connectedSockets = [];
+  wsServer.addEventListener('request', function(req) {
+    console.log("connect");
+    var socket = req.accept();
+    connectedSockets.push(socket);
+    socket.addEventListener('message', function(e) {
+      console.log("on message")
+      console.log(e);
+      for (var i = 0; i < connectedSockets.length; i++)
+        connectedSockets[i].send('pong');
+    });
+    socket.addEventListener('close', function() {
+      console.log('Client disconnected');
+      for (var i = 0; i < connectedSockets.length; i++) {
+        if (connectedSockets[i] == socket) {
+          connectedSockets.splice(i, 1);
+          break;
+        }
+      }
+    });
     return true;
   });
   server.listen(9099);
